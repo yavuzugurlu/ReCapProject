72a9928efc09241d8774d518ac4dd141e051e04f
Yavuz UĞURLU <yavuz.ugurlu06@outlook.com>
2021-08-13 02:42:04 +0300

    Part two okey

---
+++ DataAccess/Concrete/EntityFramework/ReCapContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    class ReCapContext:DbContext
+    {
+        //override on en üstteki tabloları ilişkilendirme
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocaldb;Database=ReCap;Trusted_Connection=true");
+        }
+        //prop
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+    }
+}
